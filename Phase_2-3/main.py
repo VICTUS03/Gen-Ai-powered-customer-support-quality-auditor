c70ff363a6a818d40e9c9a62495c723b1b24da6a
HRISHIKESH <hrishikeshkakade33@gmail.com>
2026-02-14 00:41:34 +0530

    Update main.py

--- Phase_2-3/main.py
+++ Phase_2-3/main.py
@@ -15,7 +15,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="MYSQL",
+        password="(db_pass)",
         database="quality_auditor"
     )
 
