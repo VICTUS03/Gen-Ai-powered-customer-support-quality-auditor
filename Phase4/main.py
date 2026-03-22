95cad37286a57f6374f927b009e208e683fc6e2a
HRISHIKESH <hrishikeshkakade33@gmail.com>
2026-03-22 20:06:34 +0530

    Mask database password in get_db_connection
    Updated database password to a secure format.

--- Phase4/main.py
+++ Phase4/main.py
@@ -54,7 +54,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="MYSQL",
+        password="****",
         database="quality_auditor"
     )
 
