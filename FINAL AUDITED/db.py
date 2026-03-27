3ec822936b145f8ea7c77f52a3fa5ca5ccd231a3
HRISHIKESH <hrishikeshkakade33@gmail.com>
2026-03-27 18:18:45 +0530

    Update db.py

--- FINAL AUDITED/db.py
+++ FINAL AUDITED/db.py
@@ -5,11 +5,11 @@ from pinecone import Pinecone
 
 # Update these to your new Postgres credentials
 DB_CONFIG = {
-    "host": os.getenv("DB_HOST", "localhost"),
-    "database": os.getenv("DB_NAME", "quality_auditor"),
-    "user": os.getenv("DB_USER", "postgres"),
-    "password": os.getenv("DB_PASSWORD", "postgre"),
-    "port": os.getenv("DB_PORT", "5432")
+    "host": os.getenv("DB_HOST", "****"),
+    "database": os.getenv("DB_NAME", "****"),
+    "user": os.getenv("DB_USER", "****"),
+    "password": os.getenv("DB_PASSWORD", "****"),
+    "port": os.getenv("DB_PORT", "****")
 }
 
 def get_pg_conn():
